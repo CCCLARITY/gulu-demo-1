414df77d4d2cea8c11c51d05e8d28eeeb9497a59
CCCLARITY <1227039581@qq.com>
2019-03-24 19:33:09 +0800

    完成button-group创建

--- src/button-group.vue
+++ src/button-group.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="g-button-group">
         <slot></slot>
     </div>
 
@@ -8,5 +8,26 @@
 
 </script>
 <style lang="scss">
+    .g-button-group{
+        display: inline-flex;
+        vertical-align: middle;
 
+        > .g-button{
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child{
+                border-bottom-left-radius: var(--border-radius);
+                border-top-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-bottom-right-radius: var(--border-radius);
+                border-top-right-radius: var(--border-radius);
+            }
+            &:hover{
+                position: relative;
+                z-index: 1;
+            }
+        }
+
+    }
 </style>
