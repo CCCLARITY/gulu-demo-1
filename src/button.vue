c312f23ff5fda5c187cf9a48d0337060fa023d20
CCCLARITY <1227039581@qq.com>
2019-03-24 17:51:48 +0800

    添加icon和icon属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,19 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+
 </template>
 
 <script>
-    export default {}
+    export default {
+        props: ['icon', 'iconPosition']         //接口，需要输入一个icon
+    }
 </script>
 
 <style lang="scss">
@@ -14,6 +24,10 @@
         border-radius: var(--border-radius);
         background: var(--button-bg);
         border: 1px solid var(--border-color);
+        display: inline-flex;
+        vertical-align: top;
+        justify-content: center;
+        align-items: center;
         &:hover{
             border-color: var(--border-color-hover);
         }
@@ -22,6 +36,26 @@
         }
         &:focus{
             outline: none;
+        }
+
+        > .icon{
+            order: 1;
+            margin-right: .3em;
+        }
+
+        > .content{
+            order: 2;
+        }
+
+        &.icon-right{
+            > .icon{
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+            > .content{
+                order: 1;
+            }
         }
     }
 </style>
