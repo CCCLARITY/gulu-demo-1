c9138e3e1f36ff45ce1f0830e7bfca285d4f20ca
CCCLARITY <1227039581@qq.com>
2019-04-21 23:45:51 +0800

    写了单元测试的4个示例

--- src/app.js
+++ src/app.js
@@ -15,3 +15,89 @@ new Vue({
         loading3: false,
     }
 })
+
+//单元测试
+import chai from 'chai';
+const expect = chai.expect;
+// icon
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    });
+    button.$mount('#test');
+
+    let useElement = button.$el.querySelector('use');
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings');
+
+    button.$el.remove();
+    button.$destroy();
+}
+// loading
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true,
+        }
+    });
+    button.$mount();
+
+    let useElement = button.$el.querySelector('use');
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading');
+
+    button.$el.remove();
+    button.$destroy();
+}
+//iconPosition
+// {
+//     const Constructor = Vue.extend(Button);
+//     const button = new Constructor({
+//         propsData: {
+//             icon: 'settings',
+//         }
+//     });
+//     button.$mount('#order');
+//
+//     let svg = button.$el.querySelector('svg');
+//     let {order} = window.getComputedStyle(svg);
+//     expect(order).to.eq("1");
+// }
+
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: "right"
+        }
+    });
+    button.$mount('#order');
+
+    let svg = button.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq("2");
+
+    button.$el.remove();
+    button.$destroy();
+}
+
+//click
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    });
+    vm.$mount();
+    vm.$on('click', function(){
+        expect(1).to.equal(1);
+    })
+
+    let button = vm.$el;
+    button.click();
+}
