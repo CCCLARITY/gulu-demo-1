44eccfe73536aae10a06abb606b5900b53ccd44b
CCCLARITY <1227039581@qq.com>
2019-07-01 22:49:56 +0800

    单元测试属性

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import Button from './button';
 import Icon from './icon';
 import ButtonGroup from './button-group';
+import Input from './input';
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 Vue.component('g-button-group', ButtonGroup);
+Vue.component('g-input', Input);
 
 new Vue({
     el: '#app',
@@ -14,109 +16,75 @@ new Vue({
         loading2: true,
         loading3: false,
     }
-})
+});
 
-//单元测试
 import chai from 'chai';
 const expect = chai.expect;
 
-import spies from 'chai-spies';
-chai.use(spies);
-
-// icon
+//单元测试
+//测试icon的名称是否正确
 {
     const Constructor = Vue.extend(Button);
     const button = new Constructor({
         propsData: {
             icon: 'settings',
-        }
-    });
-    button.$mount('#test');
-
-    let useElement = button.$el.querySelector('use');
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings');
-
-    button.$el.remove();
-    button.$destroy();
-}
-// loading
-{
-    const Constructor = Vue.extend(Button);
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings',
-            loading: true,
         }
     });
     button.$mount();
-
     let useElement = button.$el.querySelector('use');
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading');
-
-    button.$el.remove();
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-settings');
+    button.$el.remove();        //每一个单元测试结束后要把组建销毁，否则会占用内存
     button.$destroy();
 }
-//iconPosition
-// {
-//     const Constructor = Vue.extend(Button);
-//     const button = new Constructor({
-//         propsData: {
-//             icon: 'settings',
-//         }
-//     });
-//     button.$mount('#order');
-//
-//     let svg = button.$el.querySelector('svg');
-//     let {order} = window.getComputedStyle(svg);
-//     expect(order).to.eq("1");
-// }
-
+//测试loading参数
 {
     const Constructor = Vue.extend(Button);
     const button = new Constructor({
         propsData: {
             icon: 'settings',
-            iconPosition: "right"
+            loading: true
         }
     });
-    button.$mount('#order');
-
-    let svg = button.$el.querySelector('svg');
-    let {order} = window.getComputedStyle(svg);
-    expect(order).to.eq("2");
-
+    button.$mount();
+    let useElement = button.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-loading');
     button.$el.remove();
     button.$destroy();
 }
-
-//click 测试的目的是为了判断函数是否执行 安装chai-spies  函数的mock
-// {
-//     const Constructor = Vue.extend(Button);
-//     const vm = new Constructor({
-//         propsData: {
-//             icon: 'settings',
-//         }
-//     });
-//     vm.$mount();
-//     vm.$on('click', function(){
-//         expect(1).to.equal(1);
-//     })
-//
-//     let button = vm.$el;
-//     button.click();
-// }
+//测试默认情况下icon的位置
 {
+    const div = document.createElement('div');
+    document.body.appendChild(div)
     const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
+    const button = new Constructor({
         propsData: {
-            icon: 'settings',
+            icon: 'settings'
         }
     });
-    vm.$mount();
-    let spy = chai.spy(function(){});
-    vm.$on('click', spy)
-
-    let button = vm.$el;
-    button.click();
-    expect(spy).to.have.been.called();
+    button.$mount(div);
+    let svg = button.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq('1');
+    button.$el.remove();
+    button.$destroy();
+}
+//测试添加了icon-position之后icon的位置
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    });
+    button.$mount(div);
+    let svg = button.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq('2');
+    button.$el.remove();
+    button.$destroy();
 }
