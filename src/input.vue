43cad3dd20b4d14852c316d18140e36187499bfa
CCCLARITY <1227039581@qq.com>
2019-07-01 22:50:32 +0800

    单元测试

---
+++ src/input.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrapper">
+        <input type="text">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GuluInput'
+    }
+</script>
+
+<style lang="scss" scoped>
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    .wrapper{
+        > input{
+            height: $height;
+            border: 1px solid $border-color;
+            &:hover{
+                border: 1px solid $border-color-hover;
+            }
+        }
+    }
+</style>
