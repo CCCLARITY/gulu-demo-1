fba9f817c11625ddda3a8e4db610eadaacd963b6
CCCLARITY <1227039581@qq.com>
2019-07-02 22:45:20 +0800

    chai-spies测试函数

--- src/app.js
+++ src/app.js
@@ -19,6 +19,8 @@ new Vue({
 });
 
 import chai from 'chai';
+import spies from 'chai-spies';
+chai.use(spies);
 const expect = chai.expect;
 
 //单元测试
@@ -87,4 +89,21 @@ const expect = chai.expect;
     expect(order).to.eq('2');
     button.$el.remove();
     button.$destroy();
+}
+
+//测试button的click事件
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    });
+    vm.$mount();
+    let spy = chai.spy(function(){});
+    vm.$on('click', spy);       //点击按钮的时候调用间谍监听的函数，期待这个函数被执行
+    //希望这个函数被执行
+    let button = vm.$el;
+    button.click();
+    expect(spy).to.have.been.called();
 }
