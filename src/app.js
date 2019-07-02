ed104df2cd61ef00fffefc0b69e9d583c690ceeb
CCCLARITY <1227039581@qq.com>
2019-07-02 23:00:48 +0800

    catch finally

--- src/app.js
+++ src/app.js
@@ -20,90 +20,102 @@ new Vue({
 
 import chai from 'chai';
 import spies from 'chai-spies';
+
 chai.use(spies);
 const expect = chai.expect;
 
 //单元测试
-//测试icon的名称是否正确
-{
-    const Constructor = Vue.extend(Button);
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings',
-        }
+
+try {
+    //测试icon的名称是否正确
+    {
+        const Constructor = Vue.extend(Button);
+        const button = new Constructor({
+            propsData: {
+                icon: 'settings',
+            }
+        });
+        button.$mount();
+        let useElement = button.$el.querySelector('use');
+        let href = useElement.getAttribute('xlink:href');
+        expect(href).to.eq('#i-settings');
+        button.$el.remove();        //每一个单元测试结束后要把组建销毁，否则会占用内存
+        button.$destroy();
+    }
+    //测试loading参数
+    {
+        const Constructor = Vue.extend(Button);
+        const button = new Constructor({
+            propsData: {
+                icon: 'settings',
+                loading: true
+            }
+        });
+        button.$mount();
+        let useElement = button.$el.querySelector('use');
+        let href = useElement.getAttribute('xlink:href');
+        expect(href).to.eq('#i-loading');
+        button.$el.remove();
+        button.$destroy();
+    }
+    //测试默认情况下icon的位置
+    {
+        const div = document.createElement('div');
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button);
+        const button = new Constructor({
+            propsData: {
+                icon: 'settings'
+            }
+        });
+        button.$mount(div);
+        let svg = button.$el.querySelector('svg');
+        let {order} = window.getComputedStyle(svg);
+        expect(order).to.eq('1');
+        button.$el.remove();
+        button.$destroy();
+    }
+    //测试添加了icon-position之后icon的位置
+    {
+        const div = document.createElement('div');
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button);
+        const button = new Constructor({
+            propsData: {
+                icon: 'settings',
+                iconPosition: 'right'
+            }
+        });
+        button.$mount(div);
+        let svg = button.$el.querySelector('svg');
+        let {order} = window.getComputedStyle(svg);
+        expect(order).to.eq('2');
+        button.$el.remove();
+        button.$destroy();
+    }
+
+    //测试button的click事件
+    {
+        const Constructor = Vue.extend(Button);
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+            }
+        });
+        vm.$mount();
+        let spy = chai.spy(function () {
+        });
+        vm.$on('click', spy);       //点击按钮的时候调用间谍监听的函数，期待这个函数被执行
+        //希望这个函数被执行
+        let button = vm.$el;
+        button.click();
+        expect(spy).to.have.been.called();
+    }
+} catch(error){
+    window.errors = [error];
+} finally {
+    window.errors && window.errors.forEach((error)=>{
+        console.error(error.message);
     });
-    button.$mount();
-    let useElement = button.$el.querySelector('use');
-    let href = useElement.getAttribute('xlink:href');
-    expect(href).to.eq('#i-settings');
-    button.$el.remove();        //每一个单元测试结束后要把组建销毁，否则会占用内存
-    button.$destroy();
-}
-//测试loading参数
-{
-    const Constructor = Vue.extend(Button);
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings',
-            loading: true
-        }
-    });
-    button.$mount();
-    let useElement = button.$el.querySelector('use');
-    let href = useElement.getAttribute('xlink:href');
-    expect(href).to.eq('#i-loading');
-    button.$el.remove();
-    button.$destroy();
-}
-//测试默认情况下icon的位置
-{
-    const div = document.createElement('div');
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button);
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    });
-    button.$mount(div);
-    let svg = button.$el.querySelector('svg');
-    let {order} = window.getComputedStyle(svg);
-    expect(order).to.eq('1');
-    button.$el.remove();
-    button.$destroy();
-}
-//测试添加了icon-position之后icon的位置
-{
-    const div = document.createElement('div');
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button);
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings',
-            iconPosition: 'right'
-        }
-    });
-    button.$mount(div);
-    let svg = button.$el.querySelector('svg');
-    let {order} = window.getComputedStyle(svg);
-    expect(order).to.eq('2');
-    button.$el.remove();
-    button.$destroy();
 }
 
-//测试button的click事件
-{
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings',
-        }
-    });
-    vm.$mount();
-    let spy = chai.spy(function(){});
-    vm.$on('click', spy);       //点击按钮的时候调用间谍监听的函数，期待这个函数被执行
-    //希望这个函数被执行
-    let button = vm.$el;
-    button.click();
-    expect(spy).to.have.been.called();
-}
