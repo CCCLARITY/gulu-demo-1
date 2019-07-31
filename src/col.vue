aa771f0675246460b8b9f771ec49c2de797e9e57
CCCLARITY <1227039581@qq.com>
2019-07-31 21:10:40 +0800

    row和col的样式优化

---
+++ src/col.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="col" :class="colClass"
+        :style="colStyle"
+    >
+        <div style="border: 1px solid green">
+            <slot></slot>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'GuluCol',
+        props: {
+            span: [Number, String],
+            offset: {
+                type: [Number, String],
+            }
+        },
+        data(){
+            return {
+                gutter: 0,
+
+            }
+        },
+        computed: {
+            colClass(){
+                let {span, offset} = this;
+                return [`col-${span}`, offset && `offset-${offset}`]
+            },
+            colStyle(){
+                return {
+                    paddingLeft: this.gutter/2 + 'px',
+                    paddingRight: this.gutter/2 + 'px',
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .col{
+        height: 100px;
+        /*background: grey;*/
+        width: 50%;
+        /*border: 1px solid red;*/
+        padding: 0 10px;
+        $class: col-;
+
+        @for $n from 1 through 24{
+            &.#{$class}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+
+        $class: offset-;
+
+        @for $n from 1 through 24{
+            &.#{$class}#{$n} {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+
+    }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row" :style="rowStyle">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'GuluRow',
+        props: {
+            gutter: {
+                type: [Number, String],
+            }
+        },
+        created() {
+        },
+        mounted(){
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter;
+            });
+        },
+        computed: {
+            rowStyle (){
+                return {marginLeft: -this.gutter/2+'px', marginRight: -this.gutter/2 + 'px'}
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .row{
+        display: flex;
+        margin: 0 -10px;
+    }
+</style>
